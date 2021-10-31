e3255c0bdc76724e463354087cea864936d7a509
Fenix Sampson <Fenix.sampson@gmail.com>
2021-10-31 14:58:10 -0400

    added route for deleting post

--- controllers/api/postRoutes.js
+++ controllers/api/postRoutes.js
@@ -43,3 +43,17 @@ router.put("/:id", async (req, res) => {
         res.status(500).json(err);
     }
 });
+//route for deleting post
+router.delete("/:id", async (req, res) => {
+    try {
+        const deletedPost = await Post.destroy({
+            where: {
+                id: req.params.id,
+            }
+        });
+    } catch (err) {
+        res.status(500).json(err);
+    }
+});
+
+module.exports = router;
