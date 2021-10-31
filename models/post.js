01a4207ccc31d1f946736b21f9e6efb788bcda75
Fenix Sampson <Fenix.sampson@gmail.com>
2021-10-31 15:33:23 -0400

    completed models/post.js

---
+++ models/post.js
@@ -0,0 +1,40 @@
+const { Model, DataTypes } = require("sequelize");
+const sequelize = require("../config/connection");
+
+// sequelize Post model
+class Post extends Model {}
+
+Post.init(
+    {
+        id: {
+            type: DataTypes.INTEGER,
+            allowNull: false,
+            primaryKey: true,
+            autoIncrement: true,
+        },
+        title: {
+            type: DataTypes.STRING,
+        },
+        content: {
+            type: DataTypes.TEXT,
+        },
+        dateCreated: {
+            type: DataTypes.dateCreated,
+            allowNull: false,
+            defaultValue: DataTypes.NOW
+        },
+        userId: {
+            type: DataTypes.INTEGER,
+            references: {
+                model: "users",
+                key: "id",
+            },
+        },
+    },
+    {
+        sequelize,
+        modelName: "post",
+    }
+);
+
+model.exports = Post;
